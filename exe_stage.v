704d1ee09aa565f7cabc99da489281cca21a6663
BaekHyunWoo <moo1008@naver.com>
2024-04-30 13:21:34 +0900

    update

--- exe_stage.v
+++ exe_stage.v
@@ -1,58 +1,89 @@
 `include "param.v"
-module exe_stage(clk,rst_n,i_PCSrcE,i_RegWriteE,i_MemtoRegE,i_MemWriteE,
-		i_ALUControlE,i_BranchE,i_ALUSrcE,i_FlagWriteE,i_CondE,i_FlagsE,
-		i_RD1,i_RD2,i_ALUResultM,i_ResultW,i_ForwardAE,i_ForwardBE,i_Extend,i_Rd,
-		o_PCSrc,o_RegWrite,o_MemtoReg,o_MemWrite,o_BranchTakenE,
-		o_ALUResultE,o_WriteDataE,o_WA3E,o_Flags);
+module exe_stage(clk,rst_n,
+				i_PCSrc1E,i_RegWrite1E,i_MemtoReg1E,i_MemWrite1E,i_ALUControl1E,i_Branch1E,i_ALUSrc1E,i_FlagWrite1E,i_Cond1E,i_Flags1E,
+				i_PCSrc2E,i_RegWrite2E,i_MemtoReg2E,i_MemWrite2E,i_ALUControl2E,i_Branch2E,i_ALUSrc2E,i_FlagWrite2E,i_Cond2E,i_Flags2E,
+				i_RD1,i_RD2,i_ALUResult1M,i_Result1W,i_Forward1AE,i_Forward1BE,i_Extend_1,i_Rd1,
+				i_RD3,i_RD4,i_ALUResult2M,i_Result2W,i_Forward2AE,i_Forward2BE,i_Extend_2,i_Rd2,
+
+				o_PCSrc1,o_RegWrite1,o_MemtoReg1,o_MemWrite1,o_BranchTaken1E,
+				o_PCSrc2,o_RegWrite2,o_MemtoReg2,o_MemWrite2,o_BranchTaken2E,
+				o_ALUResult1E,o_WriteData1E,o_WA1E,o_Flags1,
+				o_ALUResult2E,o_WriteData2E,o_WA2E,o_Flags2);
 
 input clk,rst_n;
-input i_PCSrcE,i_RegWriteE,i_MemtoRegE,i_MemWriteE,i_BranchE,i_ALUSrcE,i_FlagWriteE;
-input [1:0]i_ALUControlE;
+input i_PCSrc1E,i_RegWrite1E,i_MemtoReg1E,i_MemWrite1E,i_Branch1E,i_ALUSrc1E,i_FlagWrite1E,i_PCSrc2E,i_RegWrite2E,i_MemtoReg2E,i_MemWrite2E,i_Branch2E,i_ALUSrc2E,i_FlagWrite2E;
+input [1:0]i_ALUControl1E,i_ALUControl2E;
 
-input [3:0] i_CondE,i_FlagsE;
-input [`D_WIDTH-1:0] i_RD1,i_RD2,i_ALUResultM,i_ResultW,i_Extend;
-input [1:0] i_ForwardAE,i_ForwardBE;
-input [3:0] i_Rd;
+input [3:0] i_Cond1E,i_Flags1E,i_Cond2E,i_Flags2E;
+input [`D_WIDTH-1:0] i_RD1,i_RD2,i_RD3,i_RD4,i_ALUResult1M,i_Result1W,i_Extend_1,i_ALUResult2M,i_Result2W,i_Extend_2;
+input [1:0] i_Forward1AE,i_Forward1BE,i_Forward2AE,i_Forward2BE;
+input [3:0] i_Rd1,i_Rd2;
 
-output o_PCSrc,o_RegWrite,o_MemtoReg,o_MemWrite,o_BranchTakenE;
-output [`D_WIDTH-1:0] o_ALUResultE,o_WriteDataE;
-output [3:0] o_WA3E;
-output [3:0] o_Flags;
+output o_PCSrc1,o_RegWrite1,o_MemtoReg1,o_MemWrite1,o_BranchTaken1E,o_PCSrc2,o_RegWrite2,o_MemtoReg2,o_MemWrite2,o_BranchTaken2E;
+output [`D_WIDTH-1:0] o_ALUResult1E,o_WriteData1E,o_ALUResult2E,o_WriteData2E;
+output [3:0] o_WA1E,o_WA2E;
+output [3:0] o_Flags1,o_Flags2;
 
-wire [`D_WIDTH-1:0] SrcAE;
-wire [`D_WIDTH-1:0] SrcBE;
-wire [3:0] w_ALUFlags;
-wire w_CondExE;
-
-assign SrcAE = (i_ForwardAE == `FORWARD_FROM_ID) ? i_RD1 :
-		(i_ForwardAE == `FORWARD_FROM_WB) ? i_ResultW :
-		(i_ForwardAE == `FORWARD_FROM_MEM) ? i_ALUResultM : i_RD1;
-
-assign o_WriteDataE = (i_ForwardBE == `FORWARD_FROM_ID) ? i_RD2 :
-		(i_ForwardBE == `FORWARD_FROM_WB) ? i_ResultW :
-		(i_ForwardBE == `FORWARD_FROM_MEM) ? i_ALUResultM : i_RD1;
-
-assign SrcBE = i_ALUSrcE ? i_Extend : o_WriteDataE;
-
-assign o_WA3E = i_Rd;
+wire [`D_WIDTH-1:0] SrcA1E,SrcB1E,SrcA2E,SrcB2E;
+wire [3:0] w_ALUFlags1,w_ALUFlags2;
+wire w_CondExE1,w_CondExE1;
 
 
+// ALU1
+assign SrcA1E = (i_Forward1AE == `FORWARD_FROM_ID) ? i_RD1 :
+		(i_Forward1AE == `FORWARD_FROM_WB) ? i_Result1W :
+		(i_Forward1AE == `FORWARD_FROM_MEM) ? i_ALUResult1M : i_RD1;
 
-alu alu(.i_A(SrcAE), .i_B(SrcBE), .i_Control(i_ALUControlE),
-	 .o_Result(o_ALUResultE), .o_Flag(w_ALUFlags));
+assign o_WriteData1E = (i_Forward1BE == `FORWARD_FROM_ID) ? i_RD2 :
+		(i_Forward1BE == `FORWARD_FROM_WB) ? i_Result1W :
+		(i_Forward1BE == `FORWARD_FROM_MEM) ? i_ALUResult1M : i_RD1;
+
+assign SrcB1E = i_ALUSrc1E ? i_Extend_1 : o_WriteData1E;
+
+assign o_WA1E = i_Rd1;
+
+//ALU2
+assign SrcA2E = (i_Forward2AE == `FORWARD_FROM_ID) ? i_RD3 :
+		(i_Forward2AE == `FORWARD_FROM_WB) ? i_Result2W :
+		(i_Forward2AE == `FORWARD_FROM_MEM) ? i_ALUResult2M : i_RD3;
+
+assign o_WriteData2E = (i_Forward2BE == `FORWARD_FROM_ID) ? i_RD4 :
+		(i_Forward2BE == `FORWARD_FROM_WB) ? i_Result2W :
+		(i_Forward2BE == `FORWARD_FROM_MEM) ? i_ALUResult2M : i_RD3;
+
+assign SrcB2E = i_ALUSrc2E ? i_Extend_2 : o_WriteData2E;
+
+assign o_WA2E = i_Rd2;
 
 
-condition_unit Condition_unit(.i_CondE(i_CondE), .i_FlagsE(i_FlagsE),
-				 .i_ALUFlags(w_ALUFlags),.i_FlagWriteE(i_FlagWriteE),
-				 .o_Flags(o_Flags),.o_CondExE(w_CondExE));
 
-assign o_MemtoReg = i_MemtoRegE;
+alu alu1(.i_A(SrcA1E), .i_B(SrcB1E), .i_Control(i_ALUControl1E),
+	 .o_Result(o_ALUResul1E), .o_Flag(w_ALUFlags1));
+
+alu alu2(.i_A(SrcA2E), .i_B(SrcB2E), .i_Control(i_ALUControl2E),
+	 .o_Result(o_ALUResul2E), .o_Flag(w_ALUFlags2));
+
+
+condition_unit Condition_unit1(.i_CondE(i_Cond1E), .i_FlagsE(i_Flags1E),
+				 .i_ALUFlags(w_ALUFlags1),.i_FlagWriteE(i_FlagWrite1E),
+				 .o_Flags(o_Flags1),.o_CondExE(w_CondExE1));
+
+condition_unit Condition_unit2(.i_CondE(i_Cond2E), .i_FlagsE(i_Flags2E),
+				 .i_ALUFlags(w_ALUFlags2),.i_FlagWriteE(i_FlagWrite2E),
+				 .o_Flags(o_Flags2),.o_CondExE(w_CondExE2));
+
+assign o_MemtoReg1 = i_MemtoReg1E;
+assign o_MemtoReg2 = i_MemtoReg2E;
 
 // ????...
-assign o_PCSrc = (i_PCSrcE && w_CondExE);
-assign o_RegWrite = (i_RegWriteE && w_CondExE);
-assign o_MemWrite = (i_MemWriteE && w_CondExE);
-assign o_BranchTakenE = (i_BranchE && w_CondExE);
+assign o_PCSrc1 = (i_PCSrc1E && w_CondExE1);
+assign o_RegWrite1 = (i_RegWrite1E && w_CondExE1);
+assign o_MemWrite1 = (i_MemWrite1E && w_CondExE1);
+assign o_BranchTaken1E = (i_Branch1E && w_CondExE1);
 
+assign o_PCSrc2 = (i_PCSrc2E && w_CondExE2);
+assign o_RegWrite2 = (i_RegWrite2E && w_CondExE2);
+assign o_MemWrite2 = (i_MemWrite2E && w_CondExE2);
+assign o_BranchTaken2E = (i_Branch2E && w_CondExE2);
 
 endmodule
--- exe_stage_reg.v
+++ exe_stage_reg.v
@@ -1,37 +1,55 @@
 `include "param.v"
-module exe_stage_reg(clk,rst_n,i_PCSrc,i_RegWrite,i_MemtoReg,i_MemWrite,
-			i_ALUResultE,i_WriteDataE,i_WA3E,
-			o_PCSrcM,o_RegWriteM,o_MemtoRegM,o_MemWriteM,
-			o_ALUResultM,o_WriteDataM,o_WA3M);
+module exe_stage_reg(clk,rst_n,i_PCSrc1,i_RegWrite1,i_MemtoReg1,i_MemWrite1,
+							   i_PCSrc2,i_RegWrite2,i_MemtoReg2,i_MemWrite2,
+							   i_ALUResult1E,i_WriteData1E,i_WA1E,
+							   i_ALUResult2E,i_WriteData2E,i_WA2E,
+							   o_PCSrc1M,o_RegWrite1M,o_MemtoReg1M,o_MemWrite1M,o_ALUResult1M,o_WriteData1M,o_WA1M,
+							   o_PCSrc2M,o_RegWrite2M,o_MemtoReg2M,o_MemWrite2M,o_ALUResult2M,o_WriteData2M,o_WA2M);
 
 input clk,rst_n;
-input i_PCSrc,i_RegWrite,i_MemtoReg,i_MemWrite;
-input [`D_WIDTH-1:0] i_ALUResultE,i_WriteDataE;
-input [3:0] i_WA3E;
+input i_PCSrc1,i_RegWrite1,i_MemtoReg1,i_MemWrite1,i_PCSrc2,i_RegWrite2,i_MemtoReg2,i_MemWrite2;
+input [`D_WIDTH-1:0] i_ALUResult1E,i_WriteData1E,i_ALUResult2E,i_WriteData2E;
+input [3:0] i_WA1E,i_WA2E;
 
-output reg o_PCSrcM,o_RegWriteM,o_MemtoRegM,o_MemWriteM;
-output reg [`D_WIDTH-1:0] o_ALUResultM,o_WriteDataM;
-output reg [3:0] o_WA3M;
+output reg o_PCSrc1M,o_RegWrite1M,o_MemtoReg1M,o_MemWrite1M,o_PCSrc2M,o_RegWrite2M,o_MemtoReg2M,o_MemWrite2M;
+output reg [`D_WIDTH-1:0] o_ALUResult1M,o_WriteData1M,o_ALUResult2M,o_WriteData2M;
+output reg [3:0] o_WA1M,o_WA2M;
 
 always@(posedge clk, negedge rst_n) begin
 	if(!rst_n) begin
-		o_PCSrcM <= 0;
-		o_RegWriteM <= 0;
-		o_MemtoRegM <= 0;
-		o_MemWriteM <= 0;
-		o_ALUResultM <= 0;
-		o_WriteDataM <= 0;
-		o_WA3M <= 0;
+		o_PCSrc1M <= 0;
+		o_RegWrite1M <= 0;
+		o_MemtoReg1M <= 0;
+		o_MemWrite1M <= 0;
+		o_ALUResult1M <= 0;
+		o_WriteData1M <= 0;
+		o_WA1M <= 0;
+
+		o_PCSrc2M <= 0;
+		o_RegWrite2M <= 0;
+		o_MemtoReg2M <= 0;
+		o_MemWrite2M <= 0;
+		o_ALUResult2M <= 0;
+		o_WriteData2M <= 0;
+		o_WA2M <= 0;
 	end
 
 	else begin
-		o_PCSrcM <= i_PCSrc;
-		o_RegWriteM <= i_RegWrite;
-		o_MemtoRegM <= i_MemtoReg;
-		o_MemWriteM <= i_MemWrite;
-		o_ALUResultM <= i_ALUResultE;
-		o_WriteDataM <= i_WriteDataE;
-		o_WA3M <= i_WA3E;
+		o_PCSrc1M <= i_PCSrc1;
+		o_RegWrite1M <= i_RegWrite1;
+		o_MemtoReg1M <= i_MemtoReg1;
+		o_MemWrite1M <= i_MemWrite1;
+		o_ALUResult1M <= i_ALUResult1E;
+		o_WriteData1M <= i_WriteData1E;
+		o_WA1M <= i_WA1E;
+
+		o_PCSrc2M <= i_PCSrc2;
+		o_RegWrite2M <= i_RegWrite2;
+		o_MemtoReg2M <= i_MemtoReg2;
+		o_MemWrite2M <= i_MemWrite2;
+		o_ALUResult2M <= i_ALUResult2E;
+		o_WriteData2M <= i_WriteData2E;
+		o_WA2M <= i_WA2E;
 	end
 
 end
